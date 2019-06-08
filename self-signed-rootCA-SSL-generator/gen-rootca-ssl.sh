dac814c768d7430faf8eb6596d822f3b877af67f
Vincent Pang <nzvincent@gmail.com>
2019-06-08 14:21:43 +1200

    Update gen-rootca-ssl.sh

--- self-signed-rootCA-SSL-generator/gen-rootca-ssl.sh
+++ self-signed-rootCA-SSL-generator/gen-rootca-ssl.sh
@@ -7,7 +7,7 @@
 
 BACKUP=backup-`date '+%Y%m%d%H%M%S'`
 mkdir -p ${BACKUP}
- [ -d ${BACKUP}x ] && cp -pv *.csr *.key *.crt ${BACKUP} || echo "Backup directory ${BACKUP} not found, please ensure you've write permission to current folder."; exit 127
+#[ -d ${BACKUP} ] && cp -pv *.csr *.key *.crt ${BACKUP} || echo "Backup directory ${BACKUP} not found, please ensure you've write permission to current folder."; exit 127
 
 CA_DNS=ca.example.com
 HOST_DNS=www.example.com
