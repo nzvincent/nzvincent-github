e1ca5070b40822dbe888fd6158b94297cc216c94
Vincent Pang <nzvincent@gmail.com>
2019-06-15 12:16:58 +1200

    Create samba-docker-image.sh

---
+++ docker/samba-docker-image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#@Author: 
+# This standing up a Samba server for Windows files sharing
+
+
+IMAGE=jenserat/samba-publicshare
+NAME=samba
+HOST=${name^^}
+
+RUN_OPT="-td"
+PORTMAP="-p 445:445 -p 137:137 -p 138:138 -p 139:139"
+VOLUME="-v ~/WORKSPACE:/srv -v `pwd`/CONFIG:/etc/samba \"
+EXTRA_OPT=""
+
+if [ $1 = "force" ];
+  docker rm -f samba
+fi
+
+
+
+docker run ${RUN_OPT} \
+  --name ${NAME} \
+  -h ${HOST} ${PORTMAP} ${VOLUME} ${EXTRA_OPT} \
+	${IMAGE}
