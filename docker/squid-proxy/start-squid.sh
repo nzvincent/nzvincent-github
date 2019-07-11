1ee3e2ec29b43152be7dece0eea1cdbdd76abe57
Vincent Pang <nzvincent@gmail.com>
2019-07-11 13:27:41 +1200

    Create start-squid.sh

---
+++ docker/squid-proxy/start-squid.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#  Start up squid proxy server
+# Author: nzvincent@gmail.com
+
+img=debian:jessie
+
+name=squid
+dir=`pwd`
+
+
+docker rm -f ${name}
+
+docker run -it -h ${name} --name ${name} \
+  --restart always \
+  --privileged \
+  -v /etc/localtime:/etc/localtime \
+  -v /etc/timezone:/etc/timezone \
+  -v ${dir}/CONFIG/cache:/var/cache/squid \
+  -v ${dir}/CONFIG/log:/var/log/squid \
+  --net=host \
+  ${img} \
+  /bin/sh
+
+
+# mount 2g to tmpfs to avoid firefox crashing.
+# default is 64M
+#        --mount type=tmpfs,dst=/dev/shm,tmpfs-size=1g \
+#    OR --shm-size 2g
+#  If above command not supported, login to the container to change the memory size OR include this in the entrypoint.sh file
+#  mount -o remount,size=2G /dev/shm
+
