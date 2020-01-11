9ad35aad0abd18b49f297b0573e10e5895a304a5
Vincent Pang <nzvincent@gmail.com>
2020-01-11 19:56:09 +1300

    Create startup-postfix.sh

---
+++ docker/gmail-postfix-relay/conf/startup-postfix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e 
+
+postfix stop
+
+if [ -f /etc/postfix/sasl_passwd ]; then
+  postmap /etc/postfix/sasl_passwd
+  rm -f /etc/postfix/sasl_passwd
+fi
+
+# Enable log
+# postconf maillog_file=/var/log/postfix.log 
+
+postfix start
+
+
