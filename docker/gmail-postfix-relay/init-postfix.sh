5866b0998c322135c756eac8505351ab741d9bad
Vincent Pang <nzvincent@gmail.com>
2020-01-14 22:49:03 +1300

    Update init-postfix.sh

--- docker/gmail-postfix-relay/init-postfix.sh
+++ docker/gmail-postfix-relay/init-postfix.sh
@@ -4,23 +4,24 @@
 
 set -e
 
+# [[ if true ]] && this is false || this is true 
+
 # SMTP relay host
-[[ -e $RELAYHOST ]] || export POSTFIX_RELAYHOST=$RELAYHOST && export POSTFIX_RELAYHOST="smtp.gmail.com"
-[[ -e $RELAYHOST_PORT ]] || export POSTFIX_RELAYHOST_PORT=$RELAYHOST_PORT && export POSTFIX_RELAYHOST_PORT="587"
+[[ -e $RELAYHOST ]] && export POSTFIX_RELAYHOST="smtp.gmail.com" || export POSTFIX_RELAYHOST=$RELAYHOST
+[[ -e $RELAYHOST_PORT ]] && export POSTFIX_RELAYHOST_PORT="587" || export POSTFIX_RELAYHOST_PORT=$RELAYHOST_PORT
 
 # SMTP login
-[[ -e $RELAYHOST_USER ]] || export POSTFIX_RELAYHOST_USER=$RELAYHOST_USER && export POSTFIX_RELAYHOST_USER="your-gmail-user"
-[[ -e $RELAYHOST_PASSWORD ]] || export POSTFIX_RELAYHOST_PASSWORD=$RELAYHOST_PASSWORD && export POSTFIX_RELAYHOST_PASSWORD="12345678"
+[[ -e $RELAYHOST_USER ]] && export POSTFIX_RELAYHOST_USER="dummy-gmail-user" || export POSTFIX_RELAYHOST_USER=$RELAYHOST_USER
+[[ -e $RELAYHOST_PASSWORD ]] && export POSTFIX_RELAYHOST_PASSWORD="dummy-gmail-password" || export POSTFIX_RELAYHOST_PASSWORD=$RELAYHOST_PASSWORD 
 
 # POSTFIX configuration
 # Network
-[[ -e $INET_INTERFACE ]] || export POSTFIX_INET_INTERFACE=$INET_INTERFACE && export POSTFIX_INET_INTERFACE="all"
-[[ -e $MYNETWORK_STYLE ]]|| export POSTFIX_MYNETWORK_STYLE=$MYNETWORK_STYLE && export POSTFIX_MYNETWORK_STYLE="host"
-[[ -e $MYNETWORKS ]] || export POSTFIX_MYNETWORKS=$MYNETWORKS && export POSTFIX_MYNETWORKS="all"
+[[ -e $INET_INTERFACE ]] && export POSTFIX_INET_INTERFACE="all" || export POSTFIX_INET_INTERFACE=$INET_INTERFACE 
+[[ -e $MYNETWORK_STYLE ]] && export POSTFIX_MYNETWORK_STYLE="host" || export POSTFIX_MYNETWORK_STYLE=$MYNETWORK_STYLE 
+[[ -e $MYNETWORKS ]] && export POSTFIX_MYNETWORKS="all" || export POSTFIX_MYNETWORKS=$MYNETWORKS 
 # Hostname
-[[ -e $MYMAILHOST ]] || export POSTFIX_MYMAILHOST=$MYMAILHOST && export POSTFIX_MYMAILHOST="smtp-your-domain.com"
-[[ -e $MYDOMAIN ]] || export POSTFIX_MYDOMAIN=$MYDOMAIN && export POSTFIX_MYDOMAIN="your-domain.com"
-
+[[ -e $MYMAILHOST ]] && export POSTFIX_MYMAILHOST="your-smtp-domain.com" || export POSTFIX_MYMAILHOST=$MYMAILHOST 
+[[ -e $MYDOMAIN ]] && export POSTFIX_MYDOMAIN="your-domain.com" || export POSTFIX_MYDOMAIN=$MYDOMAIN 
 
 # Environment variables substitution 
 envsubst < /root/WORKSPACE/postfix-main.cf >> /etc/postfix/main.cf 
