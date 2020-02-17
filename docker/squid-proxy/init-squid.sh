9cff39ed05514c98bfcc7f57443e84a9dc5c8298
Vincent Pang <nzvincent@gmail.com>
2020-02-17 21:09:40 +1300

    Update init-squid.sh

--- docker/squid-proxy/init-squid.sh
+++ docker/squid-proxy/init-squid.sh
@@ -4,7 +4,9 @@
 sed -i 's/()()()/\1192\.168\.1\.0\/24\3/g' /etc/squid/squid.conf
 
 # Enable disk caching
-sed -i 's/192.168.0.0\/16/192.168.1.0\/24/g' /etc/squid/squid.conf
+# FROM: #cache_dir ufs /var/cache/squid 100 16 256
+#   TO: cache_dir ufs /var/cache/squid 7000 16 256
+sed -i 's/^\#cache_dir\(\sufs\s\/var\/cache\/squid\s\)\(100\)\(.*\)/cache_dir\17000\3/g' /etc/squid/squid.conf
 
 
 
