e888359a792b4a6874c5cff459927f1eae636600
Vincent Pang <nzvincent@gmail.com>
2021-10-20 21:40:16 +1300

    Update download.sh

--- python/siteDoAutomation/download.sh
+++ python/siteDoAutomation/download.sh
@@ -3,5 +3,8 @@
 wget https://raw.githubusercontent.com/nzvincent/nzvincent-github/master/python/siteDoAutomation/Dockerfile -O Dockerfile
 wget https://raw.githubusercontent.com/nzvincent/nzvincent-github/master/python/siteDoAutomation/requirements.txt -O requirements.txt
 wget https://raw.githubusercontent.com/nzvincent/nzvincent-github/master/python/siteDoAutomation/siteDo.py -O siteDo.py
+wget https://raw.githubusercontent.com/nzvincent/nzvincent-github/master/python/siteDoAutomation/kickstart.py -O kickstart.py
 
 docker build -t "sitedo:v1" .
+
+docker run -it -v`pwd`:/myfile --name sitedo -h sitedo -w /myfile sitedo:v1 /bin/bash
